abeeab68c7457dac2cde3814cea11f245568ea44
Noga Enbar <nogaenbar@Nogas-MacBook-Pro.local>
2025-10-30 12:59:32 +0200

    fix: Conditional base path for local dev vs production

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@
   import path from 'path';
 
 export default defineConfig({
-  base: '/kix-prototype/',
+  base: process.env.NODE_ENV === 'production' ? '/kix-prototype/' : '/',
   plugins: [react()],
   resolve: {
       extensions: ['.js', '.jsx', '.ts', '.tsx', '.json'],
